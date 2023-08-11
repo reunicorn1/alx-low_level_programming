6767a826cb04e4f1218ab8cc0977565730b65c44
Reem <ireosama1@gmail.com>
2023-08-12 03:26:29 +0400

    102-print_comb5 modification 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,31 +9,23 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
 
-	/* Printing process is happening here */
-	for (i = 0; i < 10; i++)
+	/* here we will print everything */
+	for (i = 0; i < 99; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = i + 1; j < 100; j++)
 		{
-			for (k = i; k < 10; k++)
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+			putchar(' ');
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
+			if (i == 98 && j == 99)
+				putchar('\n');
+			else
 			{
-				for (l = j + 1; l < 10; l++)
-				{
-					putchar('0' + i);
-					putchar('0' + j);
-					putchar(' ');
-					putchar('0' + k);
-					putchar('0' + l);
-					if (i == 9 && j == 8 && k == 9 && l == 9)
-						putchar('\n');
-					else
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}

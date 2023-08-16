56814d9b800141c3abf98f809590f41b065065ae
Reem <ireosama1@gmail.com>
2023-08-17 01:00:45 +0400

    104-fibonacci mod 999

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,21 +2,37 @@
 
 /**
  * main - Entry point
+ * starting with 1 and 2
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
-	unsigned long int x, y, z;
+	unsigned long int x, y, z, x1, x2, y1, y2, z1, z2;
 
 	printf("1, 2");
-	for (i = 0, x = 1, y = 2; i < 96; i++)
+	for (i = 0, x = 1, y = 2; i < 90; i++)
 	{
 		z = x + y;
 		printf(", %lu", z);
 		x = y;
 		y = z;
+	}
+	x1 = x / 1000000000;
+	x2 = x % 1000000000;
+	y1 = y / 1000000000;
+	y2 = y % 1000000000;
+	for (i = 91; i < 96; i++)
+	{
+		z1 = x1 + y1;
+		z2 = x2 + y2;
+		printf(", %lu", z1 + (z2 / 1000000000));
+		printf("%lu", z2 % 1000000000);
+		x1 = y1;
+		x2 = y2;
+		y1 = z1;
+		y2 = z2;
 	}
 	printf("\n");
 	return (0);

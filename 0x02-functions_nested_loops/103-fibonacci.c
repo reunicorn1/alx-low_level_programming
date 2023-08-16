d8d37f0b90b25ef39cb02ce7e891a93206f78fe5
Reem <ireosama1@gmail.com>
2023-08-16 14:17:22 +0400

    103-fibonacci mod 1

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,23 +7,22 @@
  */
 int main(void)
 {
-	int i, j;
-	long int sum;
-	long int arr[50];
+	int i, j, sum;
+	long int arr[32];
 
 	arr[0] = 1;
 	arr[1] = 2;
 	/* Formation of the values of the array */
-	for (i = 0; i < 48; i++)
+	for (i = 0; i < 30; i++)
 	{
-		arr[i + 2] = arr[i] + arr[i + 2];
+		arr[i + 2] = arr[i] + arr[i + 1];
 	}
 	/* adding the even values less than 4 million */
-	for (j = 0, sum = 0; j < 50; j++)
+	for (j = 0, sum = 0; j < 32; j++)
 	{
-		if (arr[j] < 4000000 && arr[j] % 2 == 0)
+		if (arr[j] % 2 == 0)
 			sum += arr[j];
 	}
-	printf("%ld\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }

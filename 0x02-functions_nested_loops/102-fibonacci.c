b6bcea4de5d1693e8ac2fe8d39a30fb12783417e
Reem <ireosama1@gmail.com>
2023-08-16 13:06:53 +0400

    102-fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	long int arr[50];
+	arr[0] = 1;
+	arr[1] = 2;
+	printf("1, 2");
+	for (i = 0; i < 48; i++)
+	{
+		arr[i + 2] = arr[i] + arr[i + 1];
+		printf(", %ld", arr[i + 2]);
+	}
+
+}

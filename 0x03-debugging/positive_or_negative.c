9c963029cb7fe29589f24c6f40c11df67c51aa38
Reem <ireosama1@gmail.com>
2023-08-17 08:58:15 +0400

    0-main mod 1

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * positive_or_negative - Entry point
+ * @i: a variable which is an int
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+}

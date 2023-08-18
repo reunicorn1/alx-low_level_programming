0de90a722401362563bd79c46dc6ac9348f5fa75
Reem <ireosama1@gmail.com>
2023-08-18 15:15:14 +0400

    101-print_number mod 5

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,37 +10,16 @@
 
 void print_number(int n)
 {
-	int i, j, q, r, pwr, n1, n2;
+	unsigned int n1;
 
-	i = 10;
-	j = 1;
-	/* handling the negative values */
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		n = -n;
-	}
-	if (n < 100000000)
-	{
-		/* to find the number of digits of n */
-		while (n / i != 0)
-		{
-			j++;
-			i = i * 10;
-		}
-		/* printing process of n */
-		for (q = 1; q <= j; q++)
-		{
-			for (r = j, pwr = 1; r > q; r--)
-				pwr = pwr * 10;
-			_putchar('0' + ((n / pwr) % 10));
-		}
 	}
 	else
-	{
-		n1 = n / 100000;
-		n2 = n % 100000;
-		print_number(n1);
-		print_number(n2);
-	}
+		n1 = n;
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar('0' + (n1 % 10));
 }

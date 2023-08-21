8c0504a80dd10229bb215d0cb8d0df40321a8a0d
Reem <ireosama1@gmail.com>
2023-08-21 23:53:26 +0400

    8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	printf("%d", *a);
-	for (i = 1; i < n; i++)
-		printf(", %d", *(a + i));
-	printf("\n");
+	if (n > 1)
+	{
+		printf("%d", *a);
+		for (i = 1; i < n; i++)
+			printf(", %d", *(a + i));
+		printf("\n");
+	}
 }

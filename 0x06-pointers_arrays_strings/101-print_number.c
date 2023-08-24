a33f4750bbc0e33f3f47a074eaca84df9c486943
Reem <ireosama1@gmail.com>
2023-08-24 22:38:38 +0400

    101-print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,12 +10,17 @@
 
 void print_number(int n)
 {
+	unsigned int n1;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -(n);
+		n1 = -n;
 	}
+	else
+		n1 = n;
+
 	if (n / 10)
-		print_number(n / 10);
-	_putchar('0' + (n % 10));
+		print_number(n1 / 10);
+	_putchar('0' + (n1 % 10));
 }

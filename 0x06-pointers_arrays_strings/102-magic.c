752245f57a483972e151d30029519d6331bd9ef0
Reem Osama <ireosama1@gmail.com>
2023-08-24 13:29:23 -0700

    102-magic mod 3

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,12 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p - 4) = 98;
+  printf("%p\n", &n);
+  printf("%p\n", &a[0]);
+  printf("%p\n", &a[1]);
+  printf("%p\n", &a[2]);
+  printf("%p\n", &p);
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);

6690bc0514443d9857f23cdb7b32255d177a8e56
Reem <ireosama1@gmail.com>
2023-08-25 22:04:32 +0400

    103-infinite_add I have no idea what happened

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,39 +13,35 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-        int s1, s2, i, j, k, l, num1, num2, tem, space;
+	int s1, s2, i, j, k, l, num1, num2, tem, space;
 
-        s1 = 0;
-        s2 = 0;
-        while (n1[s1] != '\0')
-                s1++;
-        while (n2[s2] != '\0')
-                s2++;
+	s1 = 0;
+	s2 = 0;
+	while (n1[s1] != '\0')
+		s1++;
+	while (n2[s2] != '\0')
+		s2++;
 
-        i = s1 - 1;
-        j = s2 - 1;
-        space = 0;
-        for (k = size_r - 1; i >= -1 || j >= -1; i--, j--, k--)
-        {
-                num1 = (i >= 0) ? (n1[i] - '0') : 0;
-                num2 = (j >= 0) ? (n2[j] - '0') : 0;
-                tem = (num1 + num2 + space) % 10;
-                r[k] = tem + 48;
-                space = (num1 + num2 + space) / 10;
-        }
+	k = size_r;
+	space = 0;
+	for (i = s1 - 1, j = s2 - 1; i >= 0 || j >= 0 || space > 0; i--, j--)
+	{
+		num1 = (i >= 0) ? (n1[i] - '0') : 0;
+		num2 = (j >= 0) ? (n2[j] - '0') : 0;
+		tem = (num1 + num2 + space) % 10;
+		space = (num1 + num2 + space) / 10;
 
-        if (space > 0 && k >= 0)
-        {
-                 r[k] = space + '0';
-                 return (r);
-        }
+		if (k > 0)
+		{
+			k--;
+			r[k] = tem + 48;
+		}
+	}
+	if (k <= 0)
+		return (0);
 
-        if (k < 0)
-                return (0);
-
-        for (l = 0; l < size_r - k; l++)
-                r[l] = r[k + 1 + l];
-        r[l] = '\0';
-
-        return (r);
+	for (l = 0; l < size_r - k; l++)
+		r[l] = r[k + l];
+	r[l] = '\0';
+	return (r);
 }

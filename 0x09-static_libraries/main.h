ae21ea7b84557c827c055626bdbc13bb6e484eed
Reem <ireosama1@gmail.com>
2023-09-01 14:57:36 +0400

    libmy.a

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,15 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
-int _putchar(char c);
+int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-int _atoi(char *s);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-void _puts(char *str);
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -20,6 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int _abs(int n);
 
 #endif

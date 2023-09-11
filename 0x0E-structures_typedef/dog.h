ec65e5f921479588524e2bdad8270ea0086dc084
Reem <ireosama1@gmail.com>
2023-09-11 23:55:06 +0400

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog description structure
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ *
+ * Description: A Dog Description Structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

e2ac4e383427ebb0dbd5b068e7939d364f578481
Reem <ireosama1@gmail.com>
2023-09-12 02:32:33 +0400

    4-new_dog.c mod 1

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,11 +22,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	new_dog->name = strdup(name);
 	new_dog->owner = strdup(owner);
-	if  (new_dog->name == NULL || new_dog->owner == NULL)
+	if  (new_dog->name == NULL)
 	{
 		free(new_dog);
+		return (NULL);
+	}
+	if  (new_dog->owner == NULL)
+	{
 		free(new_dog->name);
-		free(new_dog->owner);
+		free(new_dog);
 		return (NULL);
 	}
 	new_dog->age = age;

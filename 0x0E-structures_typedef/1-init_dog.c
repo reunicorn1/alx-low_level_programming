a01afdaa87f3581120049458ab76d3623b3662e1
Reem <ireosama1@gmail.com>
2023-09-12 00:59:01 +0400

    a dog is the only thing mod 6

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -17,7 +17,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		exit(1);
-	d->name = name;
+	strcpy(d->name, name);
 	d->age = age;
-	d->owner = owner;
+	strcpy(d->owner, owner);
 }

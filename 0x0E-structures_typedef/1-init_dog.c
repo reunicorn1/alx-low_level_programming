d2cb313e8586807d932f5e61e173de33ae8ec188
Reem <ireosama1@gmail.com>
2023-09-12 00:11:58 +0400

    a dog is the only thing that loves you more than yourself

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initilises a variable of type struct dog
+ * @d: stucture dog
+ * @name: a string which is a pointer of a char
+ * @age: a vaiable of float type
+ * @owner: a string which is a pointer of a char
+ * Description: this function initilises a variable of type struct dog
+ * Return: non
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

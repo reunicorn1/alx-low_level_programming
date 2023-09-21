812d1798533d92cd29aea20c3e887c6668240dc7
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-09-22 00:38:01 +0400

    The hare and the tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * first - this function prints things before the main
+ *
+ * Return: Nothing.
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

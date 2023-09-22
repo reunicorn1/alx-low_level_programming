52ad5dd5ed7d875c0bead6e8b964ff3daf60fbaf
Reem Osama <ireosama1@gmail.com>
2023-09-21 23:51:21 -0700

    fix task 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -4,7 +4,7 @@
 ;     nasm -felf64 hola.asm && gcc hola.o && ./a.out
 ; ----------------------------------------------------------------------------------------
 section .data
-format db "Hello Holberton", 10, 0  ; The format string for printf
+format db "Hello, Holberton", 10, 0  ; The format string for printf
 
 section .text
 global main
@@ -15,5 +15,5 @@ main:
     mov rdi, format         ; Load the format string into rdi
     call printf            ; Call printf with the format string
     pop rbp                 ; Restore the base pointer
-    ret                     ; Return from main
-
+    mov rax, 0              ; Set rax to 0 to indeicate success
+    ret                     ; Return from main	

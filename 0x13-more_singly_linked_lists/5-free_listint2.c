82c25e4dd36161e7484d122770039fb98b18ff07
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-09-25 23:38:48 +0400

    free mod 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,13 +12,17 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *ptr, *next;
+	
+	if (head == NULL)
+		return;
 
 	ptr = *head;
+
 	while (ptr != NULL)
 	{
 		next = ptr->next;
 		free(ptr);
 		ptr = next;
 	}
-	head = NULL;
+	*head = NULL;
 }

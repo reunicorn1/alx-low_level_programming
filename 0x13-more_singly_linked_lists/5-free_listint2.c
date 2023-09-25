d3e63cc357a6b0fdbd46dd7f95076ed9d7f1b700
Reem Osama <ireosama1@gmail.com>
2023-09-25 11:52:08 -0700

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a lisint_t list
+ * @head: the pointer to the head of the list
+ *
+ * Return: Nothing.
+ */
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}
+/**
+ * free_listint2 - frees a lisint_t list
+ * @head: the pointer to the head of the list
+ *
+ * Return: Nothing.
+ */
+
+void free_listint2(listint_t **head)
+{
+	free_listint(*head);
+	*head = NULL;
+}

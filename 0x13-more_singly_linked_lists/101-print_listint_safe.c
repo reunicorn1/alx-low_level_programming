5f72dae724be1b4792f4a9909d1e74c33ec1fb59
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-09-27 11:42:13 +0400

    print safe version mod 2

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -21,7 +21,7 @@ size_t print_listint_safe(const listint_t *head)
 		{
 			if (head->next == &flag)
 			{
-				printf("-> [%p] %d\n",(void *)head, head->n);
+				printf("-> [%p] %d\n", (void *)head, head->n);
 				break;
 			}
 			printf("[%p] %d\n", (void *)head, head->n);

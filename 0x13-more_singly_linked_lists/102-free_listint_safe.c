a73a265fe70207b78baebc029ea3b6ec0dffb896
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-09-28 13:16:56 +0400

    free safe version

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_safe - frees a linked list safely
+ * @h: a pointer to the head of the list
+ *
+ * Return: the size of the list was free'd
+ */
+
+size_t free_safe(listint_t *h)
+{
+	size_t count = 0;
+
+	if (h && (h > h->next))
+	{
+		count = 1 + free_safe(h->next);
+		free(h);
+	}
+	return (count);
+}
+
+/**
+ * free_listint_safe - frees a linked list safely
+ * @h: a pointer to the head of the list
+ *
+ * Return: the size of the list was free'd
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t num = 0;
+
+	if (h)
+	{
+		num = free_safe(*h);
+		*h = NULL;
+	}
+	return (num);
+}

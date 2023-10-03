c6d9e48c66aa8128f0023ae898c881b6df57fdf5
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-10-03 13:03:07 +0400

    cp mod 2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,81 +5,100 @@
 #include <unistd.h>
 
 /**
- * copyfile_to_file - copies the contents of a file to another file
- * @file_from: the name of the file to copy contents from
- * @file_to: the name of the file to copy conents to
+ * close_file - closes files
+ * @fd: the file descriptor required
  *
- * Return: 1 on success and -1 on failure
+ * Return: Nothing.
  */
 
-int copyfile_to_file(const char *file_from, const char *file_to)
+void close_file(int fd)
+{
+	int n;
+
+	n = close(fd);
+	if (n == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * create_buffer - creates a buffer for the text from file_from
+ *
+ * Return: the buffer created
+ */
+
+char *create_buffer(void)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024 + 1);
+	if (buffer == NULL)
+		return (NULL);
+	return (buffer);
+}
+
+/**
+ * read_write - reads from file_from and writes into file_to
+ * @file_from: the file in which we read from
+ * @file_to: the file in which we write to
+ *
+ * Return: 1 in success, -1 in failure
+ */
+
+int read_write(const char *file_from, const char *file_to)
 {
 	int fd1, fd2, r, w;
-	char buffer[1024];
+	char *buffer;
 
-	if (file_from == NULL || file_to == NULL)
+	buffer = create_buffer();
+	if ((buffer == NULL) || (file_from == NULL) | (file_to == NULL))
 		return (-1);
 	fd1 = open(file_from, O_RDONLY);
-	r = read(fd1, buffer, 1024);
-	if (fd1 == -1 || r == -1)
-	{
-		close(fd1);
-		return (8);
-	}
-	close(fd1);
-	if (fd1 == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd1);
-		return (10);
-	}
 	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	w = write(fd2, buffer, r);
-	if (fd2 == -1 || w == -1)
-	{
-		close(fd2);
-		return (9);
-	}
-	if (w != r)
-		return (-1);
-	close(fd2);
-	if (fd2 == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd2);
-		return (10);
-	}
+	do {
+		r = read(fd1, buffer, 1024);
+		if (fd1 == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			free(buffer);
+			close_file(fd1);
+			close_file(fd2);
+			exit(98);
+		}
+		w = write(fd2, buffer, r);
+		if (fd2 == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			free(buffer);
+			close_file(fd1);
+			close_file(fd2);
+			exit(99);
+		}
+		fd2 = open(file_to, O_WRONLY | O_APPEND);
+	} while (r > 0);
+	free(buffer);
+	close_file(fd1);
+	close_file(fd2);
 	return (1);
 }
+
 /**
- * main - enters arguments from the commandline
+ * main - Entry point
  * @ac: the number of arguments
- * @av: the strings which are the arguments
+ * @av: argument strings
  *
- * Return: always 0.
+ * Return: always 0
  */
 
 int main(int ac, char **av)
 {
-	int res;
-
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	res = copyfile_to_file(av[1], av[2]);
-	switch (res)
-	{
-		case 8:
-			dprintf(2, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		case 9:
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		case 10:
-			exit(100);
-		default:
-			return (0);
-
-	}
+	read_write(av[1], av[2]);
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,8 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int copyfile_to_file(const char *file_from, const char *file_to);
-
+void close_file(int fd);
+char *create_buffer(void);
+int read_write(const char *file_from, const char *file_to);
+char *reallocate_buffer(char *ptr, size_t size);
 #endif

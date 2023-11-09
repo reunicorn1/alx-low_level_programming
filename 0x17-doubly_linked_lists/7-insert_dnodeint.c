d73f9914d86cab3fdb09c5fba004bb706a9d85e0
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-11-10 01:35:53 +0400

    insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -65,10 +65,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp, *new;
 	int size;
 
- 	if (*h)
- 	{
- 		size = dlistint_len(*h);
- 		if (idx == 0)
+	if (*h)
+	{
+		size = dlistint_len(*h);
+		if (idx == 0)
 			return (add_dnodeint(h, n));
 		if (idx == (unsigned int)size)
 			return (add_dnodeint_end(h, n));

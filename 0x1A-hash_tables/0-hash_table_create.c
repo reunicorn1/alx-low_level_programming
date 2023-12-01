4930c4a004834e1443974a876a764ce5bd79f4c0
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2023-12-01 12:02:41 +0400

    ht = {} - betty fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,12 +13,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-
 	hash_table_t *table = malloc(sizeof(hash_table_t));
+
 	if (!table)
 		return (NULL);
 	table->size = size;
-	table->array = (hash_node_t**)malloc(sizeof(hash_node_t*) * size);
+	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 

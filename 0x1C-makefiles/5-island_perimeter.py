fb3937a3143c8e084e16e359751abb030aa6b4db
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-01-25 14:46:15 +0400

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,51 +8,20 @@ def island_perimeter(grid):
     """
     This function returns the perimeter of the island described in grid
     """
+    if not grid or not grid[0]:
+        return 0
+
     perimeter = 0
-    if len(grid):
-        # expanding the grid
-        for lst in grid:
-            lst.append(0)
-        grid.append([0] * len(grid[0]))
+    rows, cols = len(grid), len(grid[0])
 
-        length = 1
-        shape = 0
-        start = 0
-        perimeter = 0
-        base = (0, 0)
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
 
-        for lst in grid:
-            try:
-                start = lst.index(1)
-                width = lst.count(1)
-            except ValueError:
-                start = 0
-                width = 0
-            if shape and width:
-                if base != (start, width):
-                    perimeter += 2 * (base[1] + length)
-                    perimeter -= 2 * (intersection((start, width), base))
-                    base = (start, width)
-                    length = 1
-                else:
-                    length += 1
-            elif width and not shape:
-                base = (start, width)
-                shape = 1
-            elif not width and shape:
-                perimeter += 2 * (base[1] + length)
-                break
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
 
     return perimeter
-
-
-def intersection(a, b):
-    """This function calculates the amount of intersection between
-    two shapes.
-    """
-    st = max(a[0], b[0])
-    ed = min(a[0] + a[1] - 1, b[0] + b[1] - 1)
-    if st <= ed:
-        return (ed - st + 1)
-    else:
-        return 0

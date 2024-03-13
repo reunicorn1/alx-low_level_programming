13190b53fcb7c06b0a6f46ef5add9f7af7982170
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-14 00:00:06 +0400

    107-O, 108-O

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -24,6 +24,24 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
 /*________ Function Prototypes ________*/
 
 /********* Linear Search *********/
@@ -56,5 +74,12 @@ listint_t *create_list(int *array, size_t size);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 listint_t *stepping(listint_t *node, size_t step);
 listint_t *linear_search_list(listint_t *prev, listint_t *curr, int value);
+
+/********* Linear search in a skip list **********/
+void free_skiplist(skiplist_t *list);
+void init_express(skiplist_t *list, size_t size);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
